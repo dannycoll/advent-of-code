6a985584c8bf5f4ddf565d16ad5614da41fba8d6
Daniel Collins <dptc98@gmail.com>
2022-12-13 11:47:36 +0000

    Day Five

---
+++ 2022/dayFive.js
@@ -0,0 +1,77 @@
+import { getInputData } from "./utils.js";
+
+const formatStacks = (stackStrings) => {
+  let numOfStacks = stackStrings[stackStrings.length - 1].split(" ").length;
+  let stacks = [];
+  for (let i = 0; i < numOfStacks; i++) {
+    stacks.push([]);
+  }
+  for (let i = stackStrings.length - 1; i >= 0; i--) {
+    let stack = stackStrings[i].split(" ");
+    const value = "";
+    const newValue = " ";
+
+    for (let i = 0; i < stack.length - 3; i++)
+      if (stack.slice(i, i + 4).filter((item) => item == value).length === 4)
+        stack.splice(i, 4, newValue);
+
+    if (stack.length !== numOfStacks) break;
+
+    stack.forEach((item, index) =>
+      item != " " ? stacks[index].push(item) : null
+    );
+  }
+  return stacks;
+};
+
+const moveCrates = (stacks, matches, model) => {
+  const [x, y, z] = matches;
+  if (model == 9000) {
+    const temp = stacks[y - 1].slice(0, -x);
+    stacks[z - 1] = stacks[z - 1].concat(stacks[y - 1].slice(-x).reverse());
+    stacks[y - 1] = temp;
+  } else {
+    const temp = stacks[y - 1].slice(0, -x);
+    stacks[z - 1] = stacks[z - 1].concat(stacks[y - 1].slice(-x));
+    stacks[y - 1] = temp;
+  }
+};
+const dayFive = async () => {
+  const text = await getInputData(5);
+  const lines = text.split("\n");
+  let stackStrings = [];
+  const regex = /\d+/g;
+  for (const line of lines) {
+    const matches = line.match(regex);
+    if (!matches) {
+      stackStrings.push(line);
+    } else break;
+  }
+  let stacks = formatStacks(stackStrings);
+
+  for (const line of lines) {
+    const matches = line.match(regex);
+    if (line.includes("move") && matches.length === 3)
+      moveCrates(stacks, matches, 9000);
+  }
+  console.log(
+    stacks
+      .map((stack) => stack[stack.length - 1].replace("[", "").replace("]", ""))
+      .join("")
+  );
+
+  //part two
+  stacks = formatStacks(stackStrings);
+  for (const line of lines) {
+    const matches = line.match(regex);
+    if (line.includes("move") && line.match(regex).length === 3)
+      moveCrates(stacks, matches, 9001);
+  }
+  console.log(
+    stacks
+      .map((stack) => stack[stack.length - 1].replace("[", "").replace("]", ""))
+      .join("")
+  );
+};
+
+export default dayFive;
--- 2022/index.js
+++ 2022/index.js
@@ -2,9 +2,10 @@ import dayOne from "./dayOne.js";
 import dayTwo from "./dayTwo.js";
 import dayThree from "./dayThree.js";
 import dayFour from "./dayFour.js";
+import dayFive from "./dayFive.js";
 
 const main = async () => {
-  dayFour();
+  dayFive();
 };
 
 main();
